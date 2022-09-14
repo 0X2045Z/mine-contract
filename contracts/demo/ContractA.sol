37a731885993f8f8d5221ed632ac6b0bb9959956
SiriusDao-development <siriusdao911@gmail.com>
2022-09-15 00:14:44 +0800

    Added

--- contracts/demo/ContractA.sol
+++ contracts/demo/ContractA.sol
@@ -5,6 +5,16 @@ import './interfaces/InterfaceContract.sol';
 
 contract ContractA is InterfaceContract{
 
+    address public admin;
+
+    /**
+     * @dev 构造函数
+     */
+    constructor(){
+        //factory地址为合约布署者
+        admin = msg.sender;
+    }
+
     function readHello() external override pure returns(uint _a){
         return 1;
     }
--- contracts/demo/ContractB.sol
+++ contracts/demo/ContractB.sol
@@ -27,8 +27,10 @@ contract ContractB {
     }
 
     // creat2工厂合约--部署合约
+    // creat2部署无法给构造函数传参，可以在所布合约中添加init方法用于传递初始化参数
     // code--需要部署的合约的code，salt--盐值
     function deploy(bytes memory code, bytes32 salt) internal returns (address addr) {
+        // 内联会编
         assembly {
             addr := create2(0, add(code, 0x20), mload(code), salt)
             if iszero(extcodesize(addr)) { revert(0, 0) }
